23a2ffcafa46916a13cf23ae24398f7f50c51d25
MustafaAlsorkhi <mustafaalsorakhi6@gmail.com>
2023-09-29 21:41:57 +0300

    extra file

---
+++ app.js
@@ -0,0 +1,97 @@
+// var x = -9*3 ;
+// console.log(x);                       -27
+
+// var y = "value is"+50;
+// console.log(y);                     value is50
+  
+// var z = 17 % 5 ;
+// console.log(z);                         2
+
+// var zz = 5 % 17 ;
+// console.log(zz);                        5
+
+// var r = 5/10;
+// console.log(r);                        0.5
+ 
+// var c = 4
+// if(c == 4)
+// {
+//     console.log("True");               True
+// } else {
+//     console.log("Doen't Match");
+// }
+
+// var h = 4;
+// if(h !=5){
+//     console.log("True");               True
+// }else{
+//     console.log("Matches");
+// }
+
+// var j = 7 ;
+// if(j<=8)
+// {
+//     console.log("True");               True
+// } else{console.log("False");}
+
+// var x = 6;
+// var nn = Math.ceil(x)-Math.floor(x) 
+// console.log(nn);                         0     
+
+//q2
+// var num = window.prompt("Enter any number");
+// window.alert("Your number is : " +num);                 
+
+//q3
+// var num1 = window.prompt("Enter your number one");
+// var num2 = window.prompt("Enter your number two");
+// if(num1>num2){
+// window.alert("Your numbers are : " + num1 + " , " + num2); 
+// }   
+// else{
+// window.alert("Your numbers are : " + num2 + " , " + num1);    
+// }
+
+//q4
+// var num1 = window.prompt("Enter your number one");
+// var num2 = window.prompt("Enter your number two");
+// if(num1>num2){
+// window.alert("The biggest number : " +num1); 
+// }   
+// else{
+//     window.alert("The biggest number : " +num2); 
+// }
+
+//q5
+// var num1 = window.prompt("Enter your number one");
+// var num2 = window.prompt("Enter your number two");
+// var num3 = num1+num2;
+//  window.alert("The sum = " + num3);
+
+// var mynumber= window.prompt("Enter your number");
+// if(mynumber == 1){
+// window.alert("ONE")
+// } else if(mynumber == 2){
+//     window.alert("TWO")
+// } else if(mynumber == 3){
+//     window.alert("THREE")
+// }else if(mynumber == 4){
+//     window.alert("FOUR")
+// } else if(mynumber == 5){
+//     window.alert("FIVE")
+// }else if(mynumber == 6){
+//     window.alert("SIX")
+// } else if(mynumber == 7){
+//     window.alert("SEVEN")
+// }else if(mynumber == 8){
+//     window.alert("EIGHT")
+// }else if(mynumber == 9){
+//     window.alert("NINE")
+// }else{
+//     window.alert("PLEASE TRY AGAIN")
+// }
+
+
+
+
+
